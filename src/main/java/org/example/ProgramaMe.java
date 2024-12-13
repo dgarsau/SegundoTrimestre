113374a6d7a590dc65fd6b1bddc9cd61e614b1ed
A714\1dam <1garsau@gmail.com>
2024-12-13 16:30:30 +0100

    termino programame

--- src/main/java/org/example/ProgramaMe.java
+++ src/main/java/org/example/ProgramaMe.java
@@ -11,7 +11,7 @@ public class ProgramaMe {
     static java.util.Scanner entrada;
 
     /**
-     * Función principal, llama a casoDePrueba mientras devuelva true.
+     * Función main, llama a casoDePrueba mientras devuelva true.
      * @param args
      */
     public static void main(String[] args) {
@@ -21,20 +21,20 @@ public class ProgramaMe {
     }
 
     /**
-     * Desde aquí se llaman a las demás funciones
+     * Este es el método que llama a los demás, es la función principal del código.
      *
-     * @return --devuelve un booleano que depende de la entrada del usuario
-     *         --si la entrada es solo un 0, finaliza
+     * @return devuelve un booleano que depende de la entrada del usuario si la entrada es solo un 0, finaliza.
      */
     public static boolean casoDePrueba() {
 
         char[] abecedario= vectorAbecedario();
         String linea = entrada.nextLine();
-        if (linea.trim().equals("0")) {
-            return false;
-        }
 
         long[] vector = vectorSplit(linea);
+
+        if(vector.length==0 || (vector.length==1 && vector[0]==0)){
+            return false;
+        }
 
         String salida = "";
         String salidainvertida= "";
@@ -43,15 +43,15 @@ public class ProgramaMe {
         for (int i = 0; i < vector.length-1; i++) {
             long cociente = vector[i];
 
-            if (cociente==0){
-                break;
+            if (cociente<=0){
+                return false;
             }
 
             while(cociente>26){
                 int resto = (int) (cociente%26);
                 if(resto==0){
                     resto=26;
-                    cociente--;
+                    cociente--; //Esto es necesario ya que en todas las divisiones se redondea hacia abajo menos en este caso que no redondea.
                 }
                 cociente=division(cociente);
                 salida += abecedario[resto-1];
@@ -69,32 +69,29 @@ public class ProgramaMe {
     }
 
     /**
-     * Esta función convierte la entrada del usuario en un vector de tipo long mediante un
-     * split y un bucle for. También controla que la entrada sea del tipo correcto, si no
-     * lo es, finaliza el programa.
+     * Este método convierte la entrada del usuario en un vector de tipo long mediante un split y un bucle for. También controla que la entrada sea del tipo correcto, si no lo es, finaliza el programa devolviendo un vector sin tamaño.
      *
      * @param cadena
      * @return vectorInt
      */
     public static long[] vectorSplit(String cadena){
         String[] vectorString = cadena.split(" ");
-        long[] vectorInt = new long[vectorString.length];
+        long[] vectorLong = new long[vectorString.length];
 
         for (int i = 0; i < vectorString.length; i++) {
             try{
-                vectorInt[i]=Long.parseLong(vectorString[i]);
+                vectorLong[i]=Long.parseLong(vectorString[i]);
             }catch (NumberFormatException e){
-                System.exit(0);
+                vectorLong = new long[0]; //devuelvo un vector sin tamaño para salir del programa en casoDePrueba
+                return vectorLong;
             }
         }
 
-        return vectorInt;
+        return vectorLong;
     }
 
     /**
-     * Esto genera un vector de tipo char con las letras del abecedario, usaré su posición
-     * para asignar letras dependiendo del resto del número. El tipo char me permite sumar
-     * números.
+     * Esto genera un vector de tipo char con las letras del abecedario, usaré su posición para asignar letras dependiendo del resto del número. El tipo char me permite sumar números.
      *
      * @return abecedario
      */
@@ -108,6 +105,7 @@ public class ProgramaMe {
 
 
     /**
+     * En esta función se divide el número recogido entre 26.
      *
      * @param cociente
      * @return cociente
@@ -117,6 +115,12 @@ public class ProgramaMe {
         return cociente;
     }
 
+    /**
+     * En esta función le doy la vuelta a una variable de tipo string convertiendola en un array con un split y recorriéndola.
+     *
+     * @param palabra
+     * @return
+     */
     public static String delReves(String palabra){
         String[] vectorpalabra = palabra.split("");
         String palabrainvertida = "";
@@ -129,3 +133,6 @@ public class ProgramaMe {
     }
 
 }
+
+
+/* Se entrega el enlace de GitHub un informe de prácticas (pruebas de JUnit aquí), el javadoc exportado en html (.zip) y el tutorial hablando*/
--- src/test/java/org/example/ProgramaMeTest.java
+++ src/test/java/org/example/ProgramaMeTest.java
@@ -6,43 +6,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProgramaMeTest {
 
-    @Test
-    void main() {
-    }
-
-    @Test
-    void casoDePrueba() {
-    }
 
     @Test
     void vectorSplit() {
+
+        long[] salida1 = {0};
+        long[] salida2 = {1, 2, 3, 4, 5, 0};
+        long[] salida3 = new long[0];
+
+        assertAll(
+                () -> assertArrayEquals(salida1,ProgramaMe.vectorSplit("0")),
+                () -> assertArrayEquals(salida2,ProgramaMe.vectorSplit("1 2 3 4 5 0")),
+                () -> assertArrayEquals(salida3,ProgramaMe.vectorSplit("hola"))
+        );
     }
 
-    @Test
-    void vectorAbecedario() {
-    }
-
+    /* Aquí no hace falta probar con letras porque la entrada está controlada en mi programa por otro método que se ejecuta antes, no pruebo entre 0 porque el programa no llega a la división si es 0*/
     @Test
     void division() {
+
+        assertAll(
+                () -> assertEquals(27, ProgramaMe.division(702)),
+                () -> assertEquals(3, ProgramaMe.division(100)),
+                () -> assertEquals(0, ProgramaMe.division(25))
+
+        );
     }
 
+    /* Como estamos operando con Strings, le dará la vuelta en todos los casos*/
     @Test
     void delReves() {
+
+        assertAll(
+                () -> assertEquals("edcba", ProgramaMe.delReves("abcde")),
+                () -> assertEquals("AAAAA", ProgramaMe.delReves("AAAAA")),
+                () -> assertEquals("54321", ProgramaMe.delReves("12345"))
+        );
     }
 }
-
-
-/*
-
-EJEMPLO
-
-assertAll(
-    () -> assertTrue(TablaMultiplicar.validarTabla(7))
-    () -> assertTrue(TablaMultiplicar.validar(-3))
-    () -> assertTrue(TablaMultiplicar.validar(57))
-
-);
-
-
-
-*/
